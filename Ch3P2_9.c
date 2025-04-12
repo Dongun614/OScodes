5bea7fdec783301fb60b4942d00ac603667baf74
Dongun614 <72856107+Dongun614@users.noreply.github.com>
2025-04-12 17:03:58 +0900

    create Ch3P2_9

---
+++ Ch3P2_9.c
@@ -0,0 +1,11 @@
+int main(){
+    if(fork() == 0){
+        printf("Child1: pid=%d pgrp=%d\n", getpid(), getpgrp());
+        if(fork() == 0){
+            printf("Child2: pid=%d pgrp=%d\n", getpid(), getpgrp());
+            while(1);
+        }
+    }
+
+    return 0;
+}
