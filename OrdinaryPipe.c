97587d91e588768e3ae47ee559f7aace18918645
Dongun614 <72856107+Dongun614@users.noreply.github.com>
2025-04-16 10:55:33 +0900

    ordinary pipe

--- OrdinaryPipe.c
+++ OrdinaryPipe.c
@@ -8,5 +8,38 @@
 #define WRITE_END 1
 
 int main(){
+    char write_msg[BUF_SIZE] = "Greeting";
+    char read_msg[BUF_SIZE];
+    pid_t pid;
+    int fd[2];
 
+    if(pipe(fd) == -1){
+        frpintf(stderr, "Pipe failed\n");
+        return 1;
+    }
+
+    pid = fork();
+
+    if(pid < 0){
+        fprintf(stderr, "Fork failed\n");
+        return 1;
+    }
+
+    if(pid > 0){
+        close(fd[READ_END]);
+
+        write(fd[WRITE_END], write_msg, strlen(write_msg)+1);
+
+        close(fd[WRITE_END]);
+    }
+    else{
+        close(fd[WRITE_END]);
+
+        read(fd[READ_END], read_msg, BUF_SIZE);
+        printf("child read %s\n", read_msg);
+
+        close(fd[READ_END]);
+    }
+
+    return 0;
 }
