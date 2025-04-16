27d1cdfa674ad7daca415b0bbc4917c195fce813
Dongun614 <72856107+Dongun614@users.noreply.github.com>
2025-04-16 10:51:06 +0900

    update Ordinary

--- OrdinaryPipe.c
+++ OrdinaryPipe.c
@@ -3,43 +3,10 @@
 #include <string.h>
 #include <sys/types.h>
 
-#define BUFFER_SIZE 25
+#define BUF_SIZE 25
 #define READ_END 0
 #define WRITE_END 1
 
 int main(){
-    char write_msg[BUFFER_SIZE] = "Greetings";
-    char read_msg[BUFFER_SIZE];
-    pid_t pid;
-    int fd[2];
 
-    if(pipe(fd) == -1){
-        fprintf(stderr, "Pipe failed\n");
-        return 1;
-    }
-
-    pid = fork();
-
-    if(pid < 0){
-        fprintf(stderr, "Fork failed");
-        return 1;
-    }
-
-    if(pid > 0){
-        close(fd[READ_END]);
-
-        write(fd[WRITE_END], write_msg, strlen(write_msg)+1);
-
-        close(fd[WRITE_END]);
-    }
-    else{
-        close(fd[WRITE_END]);
-
-        read(fd[READ_END], read_msg, BUFFER_SIZE);
-        printf("child read %s\n", read_msg);
-
-        close(fd[READ_END]);
-    }
-
-    return 0;
 }
